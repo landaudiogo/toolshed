21afea0fc2c9aa4b4ff623f655c99da6f233b96f
landaudiogo <diogo.hewitt.landau@hotmail.com>
2025-12-05 13:38:48 +0100

    webcam: add synchronisation and exit on worker exception

--- webcam/src/gunicorn.conf.py
+++ webcam/src/gunicorn.conf.py
@@ -1,10 +1,13 @@
+import sys
+
 def worker_exit(server, worker):
     """
     Called just after a worker has exited.
     :param server: The Arbiter instance (Gunicorn master process)
     :param worker: The Worker instance
     """
-    from main import motion
-    if motion:
-        motion.terminate()
+    from main import webcam
+    if webcam.motion:
+        webcam.motion.terminate()
         server.log.info("terminating motion")
+    sys.exit(4)
--- webcam/src/main.py
+++ webcam/src/main.py
@@ -1,33 +1,63 @@
+from typing import NamedTuple, Optional
+from collections import namedtuple
 from flask import Flask, render_template, request, redirect, url_for
+from threading import Lock
+from subprocess import Popen
 import subprocess
 import time
 import os
+import re
+
+
+class WebcamState:
+    state: bool
+    lock: Lock
+    motion: Optional[Popen]
+
+    def __init__(self, state: bool, lock: Lock, motion: Optional[Popen] = None):
+        self.state = state
+        self.lock = lock
+        self.motion = motion
+
 
 app = Flask(__name__)
-
-# Global variable for demonstration (in production, use a session or database)
-
-state = False
-motion = None
+webcam = WebcamState(False, Lock())
 WEBCAM_ADDRESS = os.getenv("WEBCAM_ADDRESS", "http://localhost:8081/")
+
+@app.route("/state", methods=["GET"])
+def state():
+    with webcam.lock:
+        return str(webcam.state)
 
 @app.route("/click", methods=["POST"])
 def click():
-    global state, motion, WEBCAM_ADDRESS
-    state ^= True
-    if state: 
-        motion=subprocess.Popen(["motion", "-c", "motion.conf"])
-        time.sleep(1.5)
-    elif state == False and motion != None:
-        motion.terminate()
-        motion = None
+    with webcam.lock:
+        webcam.state ^= True
+        if webcam.state: 
+            r, w = os.pipe()
+            webcam.motion = subprocess.Popen(
+                ["motion", "-c", "motion.conf"],
+                stdout=w,
+                stderr=w,
+                text=True,
+                bufsize=1
+            )
+            with os.fdopen(r, mode="r", encoding="utf-8") as pipe:
+                for line in pipe:
+                    if re.search(r"Camera \d+ started", line):
+                        break
+                time.sleep(1.5)
+        elif webcam.state == False and webcam.motion != None:
+            webcam.motion.terminate()
+            webcam.motion = None
 
     return redirect(url_for("index"))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     global state
-    return render_template("index.html", state="On" if state == True else "Off", WEBCAM_ADDRESS=WEBCAM_ADDRESS)
+    with webcam.lock:
+        return render_template("index.html", state="On" if webcam.state == True else "Off", WEBCAM_ADDRESS=WEBCAM_ADDRESS)
 
 if __name__ == "__main__":
     app.run(debug=True)
