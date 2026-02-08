81484fe05d35fc7e6dcd98c330c9b682707befa8
landaudiogo <diogo.hewitt.landau@hotmail.com>
2026-02-08 14:52:29 +0100

    pdnsctl: Wrap shell script to prefix PATH

--- pdnsctl/default.nix
+++ pdnsctl/default.nix
@@ -1,17 +1,20 @@
-{ pkgs }:
-
+{ pkgs, lib }:
+let 
+    runtimeDeps = with pkgs; [ bash curl ];
+in
 rec {
     package = pkgs.stdenv.mkDerivation {
         name = "pdnsctl";
-        buildInputs = with pkgs; [ 
-            bash 
-            curl 
-        ];
+        buildInputs = [ runtimeDeps ] ++ [ pkgs.makeWrapper ];
         dontUnpack = true;
         installPhase = ''
             install -Dm755 ${./get-rrset.sh} $out/bin/get-rrset
             install -Dm755 ${./create-rrset.sh} $out/bin/create-rrset
             install -Dm755 ${./delete-rrset.sh} $out/bin/delete-rrset
+
+            wrapProgram $out/bin/get-rrset --prefix PATH : ${lib.makeBinPath runtimeDeps}
+            wrapProgram $out/bin/create-rrset --prefix PATH : ${lib.makeBinPath runtimeDeps}
+            wrapProgram $out/bin/delete-rrset --prefix PATH : ${lib.makeBinPath runtimeDeps}
         '';
     };
     devShell = package.overrideAttrs ( prev: { 
